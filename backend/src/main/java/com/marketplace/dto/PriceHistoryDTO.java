7f73be456059c12531555fa9af998e0e90402a8a
VLADVLADYSLAVYT <vladvladislav377@gmail.com>
2026-09-08 18:29:29 +0300

    Add PriceHistoryDTO

---
+++ backend/src/main/java/com/marketplace/dto/PriceHistoryDTO.java
@@ -0,0 +1,20 @@
+package com.marketplace.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceHistoryDTO {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private BigDecimal price;
+    private BigDecimal demandIndex;
+    private BigDecimal supplyIndex;
+    private LocalDateTime recordedAt;
+}
